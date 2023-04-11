55419b0f2f0aba67c0d3417538a525956377abee
purabhi <98867198+purabhi@users.noreply.github.com>
2023-04-11 06:46:19 +0000

    dynamic fetching data

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'Title': 'Data Analyst',
+    'Location': 'Benglaru, India',
+    'Salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'Title': 'Data Scientist',
+    'Location': 'Delhi, India',
+    'Salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'Title': 'Frontend Engineer',
+    'Location': 'Remote',
+    'Salary': 'Rs. 12,00,000'
+  },
+  {
+    'id': 4,
+    'Title': 'Backend Engineer',
+    'Location': 'San Francisco, USA',
+    'Salary': 'Rs. 15,00,000'
+  },
+]
+
 
 @app.route("/")
 def hello_jovian():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 
 app.run(host='0.0.0.0', debug=True)
